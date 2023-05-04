6a2fbc6f5b889ddc0536e7b175ffb0c423afe9e7
giovannijunseokim <kingjunseo@gmail.com>
2023-05-04 13:50:58 +0900

    [chore] Main Activity 위치 변경

--- app/src/main/java/com/android/ssutudy/presentation/main/view/MainActivity.kt
+++ app/src/main/java/com/android/ssutudy/presentation/main/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.android.ssutudy
+package com.android.ssutudy.presentation.main.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.ssutudy.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
