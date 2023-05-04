8f50bba63f28e4ceb355aedf405f9228a2a81321
giovannijunseokim <kingjunseo@gmail.com>
2023-05-04 15:34:31 +0900

    [feat] LoginActivity 기능 구현

--- app/src/main/java/com/android/ssutudy/presentation/login/view/LoginActivity.kt
+++ app/src/main/java/com/android/ssutudy/presentation/login/view/LoginActivity.kt
@@ -1,12 +1,35 @@
 package com.android.ssutudy.presentation.login.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.android.ssutudy.R
+import androidx.appcompat.app.AppCompatActivity
+import com.android.ssutudy.databinding.ActivityLoginBinding
+import com.android.ssutudy.presentation.main.view.MainActivity
+import com.android.ssutudy.presentation.signup.view.SignUpActivity
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSignUpTextViewClickEvent()
+        setLoginBtnClickEvent()
+    }
+
+    private fun setLoginBtnClickEvent() {
+        binding.btnLogin.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+        if (isFinishing) finish()
+    }
+
+    private fun setSignUpTextViewClickEvent() {
+        binding.tvLoginSignUp.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
+        if (isFinishing) finish()
     }
 }
