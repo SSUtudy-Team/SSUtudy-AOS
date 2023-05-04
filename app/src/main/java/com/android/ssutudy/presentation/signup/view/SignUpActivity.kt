306c991d4ad59a7cb71153509c49899a52fe2976
giovannijunseokim <kingjunseo@gmail.com>
2023-05-04 15:32:48 +0900

    [feat] SignUpActivity init

---
+++ app/src/main/java/com/android/ssutudy/presentation/signup/view/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.android.ssutudy.presentation.signup.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.ssutudy.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
