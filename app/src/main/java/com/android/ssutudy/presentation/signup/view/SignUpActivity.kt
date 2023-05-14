dfd3bdb96c00fffc42500a314ffbb816672ab7b4
giovannijunseokim <kingjunseo@gmail.com>
2023-05-15 01:21:20 +0900

    [feat] SignUpActivity 기능 구현

--- app/src/main/java/com/android/ssutudy/presentation/signup/view/SignUpActivity.kt
+++ app/src/main/java/com/android/ssutudy/presentation/signup/view/SignUpActivity.kt
@@ -1,12 +1,42 @@
 package com.android.ssutudy.presentation.signup.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
 import com.android.ssutudy.R
+import com.android.ssutudy.databinding.ActivitySignUpBinding
+import com.android.ssutudy.listeners.signup.OnSignUpNextButtonClickListener
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity(), OnSignUpNextButtonClickListener {
+    private lateinit var binding: ActivitySignUpBinding
+    private val signUpFirstFragment by lazy { SignUpFirstFragment() }
+    private val signUpSecondFragment by lazy { SignUpSecondFragment() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        initViewBinding()
+
+        setFirstFragment(savedInstanceState)
     }
+
+    private fun initViewBinding() {
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+
+    private fun setFirstFragment(savedInstanceState: Bundle?) {
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                add(R.id.fcv_sign_up, signUpFirstFragment)
+            }
+        }
+    }
+
+    override fun setNextFragment() {
+        supportFragmentManager.commit {
+            replace(R.id.fcv_sign_up, signUpSecondFragment)
+        }
+    }
+
 }
