e7e940fb6f0b44d88760557b122700ecb6c4b7bd
giovannijunseokim <kingjunseo@gmail.com>
2023-05-22 18:41:21 +0900

    [feat] Base ViewBinding Fragment init

---
+++ app/src/main/java/com/android/ssutudy/presentation/base/BaseViewBindingFragment.kt
@@ -0,0 +1,30 @@
+package com.android.ssutudy.presentation.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseViewBindingFragment<VB : ViewBinding> : Fragment() {
+
+    private var _binding: VB? = null
+    protected val binding get() = requireNotNull(_binding) { "binding is null" }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = setBinding(inflater)
+        return binding.root
+    }
+
+    abstract fun setBinding(layoutInflater: LayoutInflater): VB
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
