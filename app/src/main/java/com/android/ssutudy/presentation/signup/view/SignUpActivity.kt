47774063daa04931409b9f43196d9860bd784601
giovannijunseokim <kingjunseo@gmail.com>
2023-05-23 16:36:20 +0900

    [chore] SignUpActivity에서 BaseActivity 상속

--- app/src/main/java/com/android/ssutudy/presentation/signup/view/SignUpActivity.kt
+++ app/src/main/java/com/android/ssutudy/presentation/signup/view/SignUpActivity.kt
@@ -1,29 +1,26 @@
 package com.android.ssutudy.presentation.signup.view
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.view.LayoutInflater
 import androidx.fragment.app.commit
 import com.android.ssutudy.R
 import com.android.ssutudy.databinding.ActivitySignUpBinding
 import com.android.ssutudy.listeners.signup.OnSignUpNextButtonClickListener
+import com.android.ssutudy.presentation.base.BaseViewBindingActivity
 
-class SignUpActivity : AppCompatActivity(), OnSignUpNextButtonClickListener {
-    private lateinit var binding: ActivitySignUpBinding
+class SignUpActivity : BaseViewBindingActivity<ActivitySignUpBinding>(),
+    OnSignUpNextButtonClickListener {
     private val signUpFirstFragment by lazy { SignUpFirstFragment() }
     private val signUpSecondFragment by lazy { SignUpSecondFragment() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initViewBinding()
 
         setFirstFragment(savedInstanceState)
     }
 
-    private fun initViewBinding() {
-        binding = ActivitySignUpBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-    }
-
+    override fun setBinding(layoutInflater: LayoutInflater): ActivitySignUpBinding =
+        ActivitySignUpBinding.inflate(layoutInflater)
 
     private fun setFirstFragment(savedInstanceState: Bundle?) {
         if (savedInstanceState == null) {
