552f39faa7996dcf3c5c119b601d3ea5cb2ae7c1
giovannijunseokim <kingjunseo@gmail.com>
2023-05-23 16:35:59 +0900

    [chore] MyPageFragment에서 BaseFragment 상속

--- app/src/main/java/com/android/ssutudy/presentation/mypage/view/MyPageFragment.kt
+++ app/src/main/java/com/android/ssutudy/presentation/mypage/view/MyPageFragment.kt
@@ -1,6 +1,16 @@
 package com.android.ssutudy.presentation.mypage.view
 
-import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.android.ssutudy.databinding.FragmentMyPageBinding
+import com.android.ssutudy.presentation.base.BaseViewBindingFragment
 
-class MyPageFragment : Fragment() {
+class MyPageFragment : BaseViewBindingFragment<FragmentMyPageBinding>() {
+    override fun setBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+    ): FragmentMyPageBinding {
+        return FragmentMyPageBinding.inflate(inflater, container, false)
+    }
+
 }
