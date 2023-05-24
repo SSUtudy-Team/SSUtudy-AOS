9f7f2266e5ef5459e309611cb064d8a77fc03c0c
giovannijunseokim <kingjunseo@gmail.com>
2023-05-24 20:07:29 +0900

    [feat] 레트로핏 객체 구현

---
+++ app/src/main/java/com/android/ssutudy/data/remote/ApiFactory.kt
@@ -0,0 +1,35 @@
+package com.android.ssutudy.data.remote
+
+import com.android.ssutudy.BuildConfig
+import com.android.ssutudy.data.remote.interceptor.TokenInterceptor
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+
+object ApiFactory {
+    
+    private val client by lazy {
+        OkHttpClient.Builder().addInterceptor(TokenInterceptor())
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level =
+                    if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
+            })
+            .build()
+    }
+
+    val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl("www.???.com")
+            .client(client)
+            .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+            .build()
+    }
+
+    inline fun <reified T> create(): T = retrofit.create<T>(T::class.java)
+}
+
+object ServicePool {
+}
