e3dfbcba652e3c5129571efc22a50049dcebed79
giovannijunseokim <kingjunseo@gmail.com>
2023-05-24 14:02:45 +0900

    [feat] HomeFragment에 BaseDataBindingFragment 적용

--- app/src/main/java/com/android/ssutudy/presentation/home/view/HomeFragment.kt
+++ app/src/main/java/com/android/ssutudy/presentation/home/view/HomeFragment.kt
@@ -1,6 +1,28 @@
 package com.android.ssutudy.presentation.home.view
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import androidx.fragment.app.viewModels
+import com.android.ssutudy.R
+import com.android.ssutudy.databinding.FragmentHomeBinding
+import com.android.ssutudy.presentation.base.BaseDataBindingFragment
+import com.android.ssutudy.presentation.home.viewmodel.HomeViewModel
 
-class HomeFragment: Fragment() {
+class HomeFragment : BaseDataBindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    private val viewModel by viewModels<HomeViewModel>()
+    override fun bindViewModelWithBinding() {
+        binding.vm = viewModel
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.spinnerHomeRecommendation.adapter =
+            ArrayAdapter.createFromResource(
+                requireContext(),
+                R.array.spinner_ssutudy_recommendation,
+                R.layout.item_sign_up_second_spinner_major
+            )
+    }
+
 }
