26138dbe119b8a2b5c96ab7440d3b84618657b00
giovannijunseokim <kingjunseo@gmail.com>
2023-05-30 15:47:12 +0900

    [feat] Deatil Activity init

---
+++ app/src/main/java/com/android/ssutudy/presentation/detail/view/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.android.ssutudy.presentation.detail.view
+
+import android.os.Bundle
+import com.android.ssutudy.R
+import com.android.ssutudy.databinding.ActivityDetailBinding
+import com.android.ssutudy.presentation.base.BaseDataBindingActivity
+
+class DetailActivity : BaseDataBindingActivity<ActivityDetailBinding>(R.layout.activity_detail) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
