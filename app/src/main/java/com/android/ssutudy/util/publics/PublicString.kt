1a2d4c979b9a77c4f78cd7c9cead2eb355ac400e
giovannijunseokim <kingjunseo@gmail.com>
2023-06-03 01:53:31 +0900

    [chore] String 리소스 추가

--- app/src/main/java/com/android/ssutudy/util/publics/PublicString.kt
+++ app/src/main/java/com/android/ssutudy/util/publics/PublicString.kt
@@ -3,4 +3,5 @@ package com.android.ssutudy.util.publics
 object PublicString {
     const val TAG = "tag"
     const val INVALID_ITEM_ACCESSED = "Invalid Item Accessed"
+    const val PREFERENCE_NAME = "auth"
 }
