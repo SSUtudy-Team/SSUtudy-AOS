2a00946122179fa6e7ec9cd691d78f8103419b14
giovannijunseokim <kingjunseo@gmail.com>
2023-06-03 14:03:23 +0900

    [ui] CreateActivity 뷰 스케치

--- app/src/main/java/com/android/ssutudy/presentation/create/view/CreateActivity.kt
+++ app/src/main/java/com/android/ssutudy/presentation/create/view/CreateActivity.kt
@@ -2,11 +2,20 @@ package com.android.ssutudy.presentation.create.view
 
 import android.os.Bundle
 import android.os.PersistableBundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.viewModels
+import com.android.ssutudy.R
+import com.android.ssutudy.databinding.ActivityCreateBinding
+import com.android.ssutudy.presentation.base.BaseDataBindingActivity
+import com.android.ssutudy.presentation.create.viewmodel.CreateViewModel
 
-class CreateActivity : AppCompatActivity() {
-
+class CreateActivity : BaseDataBindingActivity<ActivityCreateBinding>(R.layout.activity_create) {
+    private val viewModel by viewModels<CreateViewModel>()
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
+
+    }
+
+    override fun bindViewModelWithBinding() {
+        binding.vm = viewModel
     }
 }
