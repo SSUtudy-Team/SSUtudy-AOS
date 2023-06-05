fe9e87735069775d33dd0e9e7721cc738cb23dc7
giovannijunseokim <kingjunseo@gmail.com>
2023-06-05 14:27:50 +0900

    [feat] 스터디 생성 DTO 작성

---
+++ app/src/main/java/com/android/ssutudy/data/remote/model/CreateDto.kt
@@ -0,0 +1,29 @@
+package com.android.ssutudy.data.remote.model
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class RequestCreateDto(
+    @SerialName("college") val college: String,
+    @SerialName("department") val department: String,
+    @SerialName("className") val className: String,
+    @SerialName("title") val title: String,
+    @SerialName("content") val content: String,
+    @SerialName("userCount") val userCount: Int,
+    @SerialName("roomLink") val roomLink: String,
+    @SerialName("categoryCodeDto") val categoryCodeDto: List<CategoryCode>,
+) {
+    @Serializable
+    data class CategoryCode(
+        @SerialName("categoryCode") val categoryCode: String,
+    )
+}
+
+@Serializable
+data class ResponseCreateDto(
+    @SerialName("success") val success: String,
+    @SerialName("message") val message: String,
+    @SerialName("data") val data: String,
+)
