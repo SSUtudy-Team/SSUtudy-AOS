c7da6839acfa398242fda9e9d73dc0ef30022db1
giovannijunseokim <kingjunseo@gmail.com>
2023-06-07 01:55:56 +0900

    [feat] LoginDto 작성

---
+++ app/src/main/java/com/android/ssutudy/data/remote/model/LoginDto.kt
@@ -0,0 +1,19 @@
+package com.android.ssutudy.data.remote.model
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class RequestLoginDto(
+    @SerialName("studentId")
+    val studentId: String,
+    @SerialName("password")
+    val password: String,
+)
+
+@Serializable
+data class ResponseLoginDto(
+    @SerialName("data")
+    val token: String,
+)성
