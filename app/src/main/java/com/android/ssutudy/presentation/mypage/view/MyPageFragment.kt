5eba21a32527db8ade4051409108ad7cfd204c52
giovannijunseokim <kingjunseo@gmail.com>
2023-06-07 23:07:58 +0900

    [feat] my page fragment data binding으로 변환

--- app/src/main/java/com/android/ssutudy/presentation/mypage/view/MyPageFragment.kt
+++ app/src/main/java/com/android/ssutudy/presentation/mypage/view/MyPageFragment.kt
@@ -2,30 +2,41 @@ package com.android.ssutudy.presentation.mypage.view
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.android.ssutudy.R
 import com.android.ssutudy.data.local.SharedPreferences
 import com.android.ssutudy.databinding.FragmentMyPageBinding
-import com.android.ssutudy.presentation.base.BaseViewBindingFragment
+import com.android.ssutudy.presentation.base.BaseDataBindingFragment
 import com.android.ssutudy.presentation.login.view.LoginActivity
+import com.android.ssutudy.presentation.mypage.viewmodel.MyPageViewModel
 
-class MyPageFragment : BaseViewBindingFragment<FragmentMyPageBinding>() {
-    override fun setBinding(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-    ): FragmentMyPageBinding {
-        return FragmentMyPageBinding.inflate(inflater, container, false)
+class MyPageFragment : BaseDataBindingFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
+    private val viewModel by viewModels<MyPageViewModel>()
+    override fun bindViewModelWithBinding() {
+        binding.vm = viewModel
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         setClickEvents()
+        setData()
+    }
+
+    private fun setData() {
+
     }
 
     private fun setClickEvents() {
+        setCategoryClickEvent()
         setLogoutTvClickEvent()
+    }
+
+    private fun setCategoryClickEvent() {
+        binding.layoutMyPageCategory.setOnClickListener {
+
+        }
     }
 
     private fun setLogoutTvClickEvent() {
