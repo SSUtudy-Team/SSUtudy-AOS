5109f0a475658c43a562a8de0caf5a9d94c05531
giovannijunseokim <kingjunseo@gmail.com>
2023-06-07 23:04:25 +0900

    [feat] 헤더에 토큰 추가

--- app/src/main/java/com/android/ssutudy/data/remote/interceptor/TokenInterceptor.kt
+++ app/src/main/java/com/android/ssutudy/data/remote/interceptor/TokenInterceptor.kt
@@ -1,17 +1,23 @@
 package com.android.ssutudy.data.remote.interceptor
 
+import com.android.ssutudy.data.local.SharedPreferences
+import com.android.ssutudy.util.publics.PublicString.TOKEN
 import okhttp3.Interceptor
 import okhttp3.Response
 
 class TokenInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalRequest = chain.request()
-        val tokenAddedRequest = originalRequest.newBuilder()
-            .header(
-                "Authorization",
-                "123"
-            )
-            .build()
-        return chain.proceed(tokenAddedRequest)
+        val token = SharedPreferences.getString(TOKEN)
+        if (token != null) {
+            val tokenAddedRequest = originalRequest.newBuilder()
+                .header(
+                    "Authorization",
+                    token
+                )
+                .build()
+            return chain.proceed(tokenAddedRequest)
+        }
+        return chain.proceed(originalRequest)
     }
 }
