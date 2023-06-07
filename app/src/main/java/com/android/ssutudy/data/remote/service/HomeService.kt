89e39d82e14dc71c36af3e92da3d7b1485a75e7d
giovannijunseokim <kingjunseo@gmail.com>
2023-06-07 14:08:58 +0900

    [chore] Home Data 받는 서비스 작성

---
+++ app/src/main/java/com/android/ssutudy/data/remote/service/HomeService.kt
@@ -0,0 +1,12 @@
+package com.android.ssutudy.data.remote.service
+
+import com.android.ssutudy.data.remote.model.ResponseHomeDto
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface HomeService {
+    @GET("users/home/{userId}")
+    suspend fun getHomeData(
+        @Path("userId") userId: Long,
+    ): ResponseHomeDto
+}
