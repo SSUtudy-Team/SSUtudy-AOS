81f86abd419584310cc7bb41ebb4a91b5c8ae8c6
giovannijunseokim <kingjunseo@gmail.com>
2023-06-09 02:34:39 +0900

    [feat] getUserInfo GET 메서드로 변경

--- app/src/main/java/com/android/ssutudy/data/remote/service/UserInfoService.kt
+++ app/src/main/java/com/android/ssutudy/data/remote/service/UserInfoService.kt
@@ -1,11 +1,11 @@
 package com.android.ssutudy.data.remote.service
 
 import com.android.ssutudy.data.remote.model.ResponseGetUserInfo
-import retrofit2.http.POST
+import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface UserInfoService {
-    @POST("users/{userId}")
+    @GET("users/{userId}")
     suspend fun getUserInfo(
         @Path("userId") userId: String,
     ): ResponseGetUserInfo
