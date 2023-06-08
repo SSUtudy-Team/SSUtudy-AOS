f2016ca7a3805d20794bc8a3594e6d645cf92ee6
giovannijunseokim <kingjunseo@gmail.com>
2023-06-09 02:45:20 +0900

    [feat] 바텀 시트에 함수 전달

--- app/src/main/java/com/android/ssutudy/presentation/mypage/view/MyPageFragment.kt
+++ app/src/main/java/com/android/ssutudy/presentation/mypage/view/MyPageFragment.kt
@@ -9,23 +9,45 @@ import com.android.ssutudy.data.local.SharedPreferences
 import com.android.ssutudy.databinding.FragmentMyPageBinding
 import com.android.ssutudy.presentation.base.BaseDataBindingFragment
 import com.android.ssutudy.presentation.login.view.LoginActivity
+import com.android.ssutudy.presentation.mypage.model.MyPageBottomSheetDialogFragmentArgs
 import com.android.ssutudy.presentation.mypage.viewmodel.MyPageViewModel
+import com.android.ssutudy.util.extensions.makeToastMessage
+import com.android.ssutudy.util.publics.PublicString.GET_USER_DATA
 
 class MyPageFragment : BaseDataBindingFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
     private val viewModel by viewModels<MyPageViewModel>()
+
     override fun bindViewModelWithBinding() {
         binding.vm = viewModel
     }
 
+    private fun setData() {
+        viewModel.getUserData()
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         setClickEvents()
+        setObservers()
         setData()
     }
 
-    private fun setData() {
+    private fun setObservers() {
+        initErrorResponseObserver()
+        initSuccessResponseObserver()
+    }
 
+    private fun initSuccessResponseObserver() {
+        viewModel.updateInterestingCategorySuccessResponse.observe(viewLifecycleOwner) {
+            makeToastMessage(it.message)
+            setData()
+        }
+    }
+
+    private fun initErrorResponseObserver() {
+        viewModel.getErrorResponse.observe(viewLifecycleOwner) {
+            makeToastMessage(it)
+        }
     }
 
     private fun setClickEvents() {
@@ -35,7 +57,13 @@ class MyPageFragment : BaseDataBindingFragment<FragmentMyPageBinding>(R.layout.f
 
     private fun setCategoryClickEvent() {
         binding.layoutMyPageCategory.setOnClickListener {
-
+            val dialog = MyPageBottomSheetDialogFragment()
+            val args = Bundle()
+            args.putParcelable(GET_USER_DATA, MyPageBottomSheetDialogFragmentArgs {
+                viewModel.getUserData()
+            })
+            dialog.arguments = args
+            dialog.show(requireActivity().supportFragmentManager, dialog.tag)
         }
     }
 
