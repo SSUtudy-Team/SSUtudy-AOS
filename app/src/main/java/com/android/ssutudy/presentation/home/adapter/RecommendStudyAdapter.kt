800a99f637ed2aa5ebe18bee67b8a5dd7c38bff7
giovannijunseokim <kingjunseo@gmail.com>
2023-06-10 03:14:08 +0900

    [feat] 추천 스터디 클릭 이벤트 추가

--- app/src/main/java/com/android/ssutudy/presentation/home/adapter/RecommendStudyAdapter.kt
+++ app/src/main/java/com/android/ssutudy/presentation/home/adapter/RecommendStudyAdapter.kt
@@ -2,18 +2,24 @@ package com.android.ssutudy.presentation.home.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.android.ssutudy.R
 import com.android.ssutudy.data.remote.model.ResponseHomeDto.Data.RecommendStudy
 import com.android.ssutudy.databinding.ItemRecommendationSsutudyBinding
 import com.android.ssutudy.util.DiffUtilCallback
+import com.android.ssutudy.util.publics.PublicString.END
+import com.android.ssutudy.util.publics.PublicString.RECRUITING
+import com.android.ssutudy.util.publics.PublicString.RECRUITMENT_COMPLETE
 
-class RecommendStudyAdapter :
+class RecommendStudyAdapter(private val startDetailActivity: (String) -> Unit) :
     ListAdapter<RecommendStudy, RecommendStudyAdapter.RecommendViewHolder>(DiffUtilCallback<RecommendStudy>()) {
 
-    class RecommendViewHolder(private val binding: ItemRecommendationSsutudyBinding) :
-        RecyclerView.ViewHolder(binding.root) {
+    class RecommendViewHolder(
+        private val binding: ItemRecommendationSsutudyBinding,
+        private val startDetailActivity: (String) -> Unit,
+    ) : RecyclerView.ViewHolder(binding.root) {
         fun onBind(item: RecommendStudy) {
             item.apply {
                 with(binding) {
@@ -22,20 +28,43 @@ class RecommendStudyAdapter :
                     tvItemRecommendationState.text = studyStatus
                     tvItemRecommendationClass.text = className
                     tvItemRecommendationContent.text = content
-                    tvItemRecommendationParticipants.text =
-                        binding.root.context.getString(
-                            R.string.current_max_participants,
-                            curUserCount.toString(),
-                            userCount.toString()
+                    tvItemRecommendationParticipants.text = binding.root.context.getString(
+                        R.string.current_max_participants,
+                        curUserCount.toString(),
+                        userCount.toString()
+                    )
+                    root.setOnClickListener {
+                        startDetailActivity.invoke(item.studyId.toString())
+                    }
+                    when (studyStatus) {
+                        RECRUITING -> tvItemRecommendationState.setTextColor(
+                            ResourcesCompat.getColor(
+                                binding.root.resources, R.color.sky_5ec2c4, null
+                            )
                         )
+
+                        RECRUITMENT_COMPLETE -> tvItemRecommendationState.setTextColor(
+                            ResourcesCompat.getColor(
+                                binding.root.resources, R.color.blue_009bcb, null
+                            )
+                        )
+
+                        END -> tvItemRecommendationState.setTextColor(
+                            ResourcesCompat.getColor(
+                                binding.root.resources, R.color.gray_969696, null
+                            )
+                        )
+                    }
                 }
             }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecommendViewHolder {
-        val binding = ItemRecommendationSsutudyBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return RecommendViewHolder(binding)
+        val binding = ItemRecommendationSsutudyBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+        return RecommendViewHolder(binding, startDetailActivity)
     }
 
     override fun onBindViewHolder(holder: RecommendViewHolder, position: Int) {
