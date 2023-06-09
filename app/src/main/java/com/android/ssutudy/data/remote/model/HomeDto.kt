15a76babc0d553f20e6a81156b4564ea0f521d0c
giovannijunseokim <kingjunseo@gmail.com>
2023-06-10 03:40:37 +0900

    [chore] 네이밍 수정

--- app/src/main/java/com/android/ssutudy/data/remote/model/HomeDto.kt
+++ app/src/main/java/com/android/ssutudy/data/remote/model/HomeDto.kt
@@ -13,23 +13,11 @@ data class ResponseHomeDto(
     @Serializable
     data class Data(
         @SerialName("joinStudy") val joinStudy: List<JoinStudy>,
-        @SerialName("recommendStudy") val recommendStudy: List<RecommendStudy>,
+        @SerialName("recommendStudy") val recommendStudy: List<Study>,
     ) {
         @Serializable
         data class JoinStudy(
             @SerialName("title") val title: String,
-            @SerialName("studyId") val studyId: Long,
-        )
-
-        @Serializable
-        data class RecommendStudy(
-            @SerialName("department") val department: String,
-            @SerialName("className") val className: String,
-            @SerialName("title") val title: String,
-            @SerialName("content") val content: String,
-            @SerialName("userCount") val userCount: Int,
-            @SerialName("curUserCount") val curUserCount: Int,
-            @SerialName("studyStatus") val studyStatus: String,
             @SerialName("studyId") val studyId: Long,
         )
     }
